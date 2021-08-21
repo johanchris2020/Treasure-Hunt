d9d9b3cd1fc9959f62628f8dfaf1222bf0720ebe
johanchris2020 <johanchris122@gmail.com>
2021-08-21 13:20:57 +0530

    first commit

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    text("A T E D A S B A", 100,250)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue");
+    textSize(13)
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("Hint: Performs a particular task !!", 700,170)
+    text("Hint: Stores all information !!", 100,270)
+    fill("white")
+
+
+
+// add code for displaying rest of the names and the hints.
+    
+
+}
